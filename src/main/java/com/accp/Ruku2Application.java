1d5baec59990e0b2ecbac31e74f0eead36d5bc5d
Chen Li Min <2445356887@qq.com>
2018-10-17 11:13:19 +0800

    陈辉

---
+++ src/main/java/com/accp/Ruku2Application.java
@@ -0,0 +1,12 @@
+package com.accp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ruku2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ruku2Application.class, args);
+	}
+}
